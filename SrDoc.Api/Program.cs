7e3f71d9e561c534dad529de9ac5ca217a2ca095
Thomas Bastos <thomasbastos2004@gmail.com>
2025-12-26 23:01:20 -0300

    feat: configure prefix of swagger in default

--- SrDoc.Api/Program.cs
+++ SrDoc.Api/Program.cs
@@ -43,7 +43,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "SrDoc API v1");
-        c.RoutePrefix = string.Empty;
+        c.RoutePrefix = "swagger";
     });
 }
 
